eb521ee4d96c9eea5c8d5c54eb4bf0e82ec5796e
Eloi-Perez <eloi104@hotmail.com>
2021-12-21 16:07:31 +0000

    require session to save

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,3 +1,6 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { ironOptions } from '../../lib/iron-config'
+
 import { Buffer } from 'buffer'
 import { writeFile } from 'fs'
 import path from 'path'
@@ -8,27 +11,35 @@ export const config = {
     },
 }
 
-export default async function handler(req, res) {
+
+export default withIronSessionApiRoute(handler, ironOptions)
+
+async function handler(req, res) {
     if (req.method === 'POST') {
-        try {
-            const fileName = req.body.fileName
-            const data = new Uint8Array(Buffer.from(JSON.stringify(req.body.data)))
-            let pathData = path.resolve(process.cwd() + '/data/') + '/'
-            writeFile(pathData + fileName, data, (err) => {
-                if (err) {
-                    console.log(err)
-                    res.status(500).json({ error: 'Wrong route', path: pathData })
-                    res.status(500).json({ error: 'error' })
-                } else {
-                    const miliseconds = Date.parse(new Date())
-                    const backup = miliseconds + '.' + fileName
-                    writeFile(pathData + backup, data)
-                    //delete older backup + second backup
-                    res.status(200).json({ message: 'Saved!' })
-                }
-            });
-        } catch (err) {
-            res.status(500).json({ error: 'error POST: ' + err })
+        const user = await req.session.user
+        if (!user) {
+            res.status(401).json({ authorized: false })
+        } else { // Authorized
+            try {
+                const fileName = req.body.fileName
+                const data = new Uint8Array(Buffer.from(JSON.stringify(req.body.data)))
+                let pathData = path.resolve(process.cwd() + '/data/') + '/'
+                writeFile(pathData + fileName, data, (err) => {
+                    if (err) {
+                        console.log(err)
+                        res.status(500).json({ error: 'Wrong route', path: pathData })
+                        res.status(500).json({ error: 'error' })
+                    } else {
+                        const miliseconds = Date.parse(new Date())
+                        const backup = miliseconds + '.' + fileName
+                        writeFile(pathData + backup, data)
+                        //delete older backup + second backup
+                        res.status(200).json({ message: 'Saved!' })
+                    }
+                });
+            } catch (err) {
+                res.status(500).json({ error: 'error POST: ' + err })
+            }
         }
     } else {
         res.status(405).end()
