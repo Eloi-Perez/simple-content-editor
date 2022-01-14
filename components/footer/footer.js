0c47eb60e846b86c59aed650d627497ba34f6718
Eloi-Perez <eloi104@hotmail.com>
2022-01-14 12:40:24 +0000

    add content

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,9 +1,16 @@
+// import { useRouter } from 'next/router'
+
 import s from './footer.module.css'
 
 export default function Footer() {
+    // const router = useRouter()
     return (
         <footer className={s.root}>
-            <h3>This is a Footer</h3>
+            <div className={s.container}>
+                <p>Some Link</p>
+                <h3>This is a Footer</h3>
+                <p className={s.login}>some actions</p>
+            </div>
         </footer>
     )
 }
